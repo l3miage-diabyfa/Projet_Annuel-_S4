8fbbfb9d9e9610ee31aef14adac9660e9ccde851
Honzikoi <honza.doan@gmail.com>
2026-01-03 19:39:19 +0100

    CI/CD hotfix#1

--- frontend/app/tarifs/page.tsx
+++ frontend/app/tarifs/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState } from 'react';
-import Image from 'next/image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faThumbsUp, faHands } from '@fortawesome/free-solid-svg-icons';
 
